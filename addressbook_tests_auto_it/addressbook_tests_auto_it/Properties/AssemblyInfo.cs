74ec721994e5f2d98cecb1c7702041d188783e0b
semenovmxm <semenovmxm@gmail.com>
2019-09-15 13:40:08 +0300

    Лекция 8.1.
    Автоматизация десктопных приложений при помощи AutoItX

---
+++ addressbook_tests_auto_it/addressbook_tests_auto_it/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_auto_it")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_auto_it")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4c112b53-ef3a-41ca-85c2-3a453f65eecf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_auto_it/addressbook_tests_auto_it/appmanager/ApplicationManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+namespace addressbook_tests_auto_it
+{ 
+
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private AutoItX3 aux;
+        private GroupHelper groupHelper;
+
+        public ApplicationManager()
+        {
+            aux = new AutoItX3();
+            groupHelper = new GroupHelper(this);
+            aux.Run(@"C:\Users\M\Downloads\FreeAddressBookPortable\AddressBook.exe", "", aux.SW_SHOW);
+            aux.WinWait(WINTITLE);
+            aux.WinActivate(WINTITLE);
+            aux.WinActive(WINTITLE);
+        }
+
+        public void Stop()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d510");  
+        }
+
+        public AutoItX3 Aux
+        {
+            get
+            {
+                return aux;
+            }
+        }
+        public GroupHelper Groups
+        {
+            get
+            {
+                return groupHelper;
+            }
+        }
+    }
+}
